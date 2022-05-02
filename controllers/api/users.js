d669e5a22f19b66dd13c5ac4af36631a041df367
RaulL10 <lopezraul1410@gmail.com>
2022-05-02 10:52:54 -0700

    created user

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,12 +1,30 @@
+const User = '../../models/user'
+const jwt = require('jsonwebtoken')
+
+
 module.exports = {
     create
 }
 
-function create(req, res) {
-    res.json({
-        user: {
-            name: req.body.name,
-            email: req.body.email
-        }
-    })
+async function create(req, res) {
+    try {
+        // add ther user to the db
+        const user = await User.create(req.body)
+        const token = createJWT(user)
+        res.json(token)
+    } catch (err) {
+        // client will chek for any non 200 code
+        res.status(400).json(err) 
+    }
 }
+
+/*-- Helper Functions --*/
+
+function createJWT(user) {
+    return jwt.sign(
+      // data payload
+      { user },
+      process.env.SECRET,
+      { expiresIn: '24h' }
+    );
+  }
